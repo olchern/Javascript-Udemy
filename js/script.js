9b9e389b19b2333e5357268704c30e3e361cf732
olchern <olchern121999@gmail.com>
2019-12-05 17:24:50 +0300

    Fifth homework

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+'use strict';
+
+let beginStart = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearSavingsValue = document.getElementsByClassName('myearsavings-value')[0],
+
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    expensesItemBtn = document.getElementsByTagName('button')[0],
+    optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBudgetBtn = document.getElementsByTagName('button')[2],
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    chooseSum = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
 let money, time;
 
 function start() {
@@ -57,7 +82,7 @@ let appData = {
     chooseOptExpenses: function () {
         for (let i = 0; i < 3; i++) {
             let optExpenses = prompt('Статья необязательных расходов');
-            appData.optionalExpenses[i + 1] = optExpenses;
+            appData.optionalExpenses[i] = optExpenses;
         }
     },
     chooseIncome: function () {
